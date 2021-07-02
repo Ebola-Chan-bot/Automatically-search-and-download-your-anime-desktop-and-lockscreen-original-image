ac53f6c6242c6dcead23b9c4283f80ec0a7f769d
埃博拉酱 <Leenrung@outlook.com>
2021-07-02 15:14:12 +0800

    升级到VS2022

---
+++ 自动搜索下载你的二次元桌面和锁屏原图/后台任务.vb
@@ -0,0 +1,136 @@
+﻿Imports Microsoft.Win32
+Imports Windows.ApplicationModel
+Imports System.Threading
+
+Module 后台任务
+	Property 日志流 As IO.StreamWriter
+	Sub 设置后台任务()
+		Const 启动参数 As String = "后台任务", 应用标识 As String = "自动搜索下载你的桌面和锁屏原图"
+		Static 任务服务 As TaskScheduler.TaskService = TaskScheduler.TaskService.Instance, 计划任务 As TaskScheduler.Task, 开机启动 As StartupTask = StartupTask.GetAsync("自启动任务").GetResults, 定时器 As New Timer(AddressOf 定时任务), 启动路径 As String = Process.GetCurrentProcess.MainModule.FileName
+		Select Case 设置项.每隔时间单位
+			Case 时间单位.关闭
+				定时器.Change(Timeout.Infinite, Timeout.Infinite)
+				开机启动.Disable()
+				If 计划任务 IsNot Nothing Then
+					计划任务.Enabled = False
+				End If
+			Case 时间单位.分钟
+				Dim 时间间隔 As TimeSpan = TimeSpan.FromMinutes(时间值(时间单位.分钟)(设置项.每隔时间值))
+				Dim 距离上次 As TimeSpan = Now - 设置项.上次任务时日
+				If 时间间隔 > 距离上次 Then
+					定时器.Change(时间间隔 - 距离上次, 时间间隔)
+				Else
+					定时器.Change(TimeSpan.Zero, 时间间隔)
+				End If
+				If 计划任务 IsNot Nothing Then
+					计划任务.Enabled = False
+				End If
+				Call 开机启动.RequestEnableAsync()
+			Case 时间单位.小时
+				Dim 时间间隔 As TimeSpan = TimeSpan.FromMinutes(时间值(时间单位.小时)(设置项.每隔时间值))
+				Dim 距离上次 As TimeSpan = Now - 设置项.上次任务时日
+				If 时间间隔 > 距离上次 Then
+					定时器.Change(时间间隔 - 距离上次, 时间间隔)
+				Else
+					定时器.Change(TimeSpan.Zero, 时间间隔)
+				End If
+				If 计划任务 IsNot Nothing Then
+					计划任务.Enabled = False
+				End If
+				Call 开机启动.RequestEnableAsync()
+			Case 时间单位.天
+				定时器.Change(Timeout.Infinite, Timeout.Infinite)
+				开机启动.Disable()
+				If IsNothing(计划任务) Then
+					计划任务 = 任务服务.AddTask(应用标识, New TaskScheduler.DailyTrigger(时间值(时间单位.天)(设置项.每隔时间值)), New TaskScheduler.ExecAction(启动路径, 启动参数))
+				End If
+				计划任务.Enabled = True
+		End Select
+	End Sub
+	Sub 写日志(内容 As String)
+		If 日志流 IsNot Nothing Then
+			日志流.WriteLine(Now.ToString & " " & 内容)
+			日志流.Flush()
+		End If
+	End Sub
+	Private Sub 发通知(通知 As Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder)
+		If 设置项.后台任务异常时发送通知 Then
+			通知.Show()
+		End If
+	End Sub
+	Async Sub 定时任务()
+		Dim 路径 As String = 搜索桌面壁纸(设置项.当前桌面路径)
+		Dim 通知文本 As String
+		If String.IsNullOrEmpty(路径) Then
+			发通知(桌面壁纸搜索失败通知)
+			写日志(桌面 & 无法定位当前壁纸)
+		Else
+			Try
+				Select Case Await 搜索并筛选原图(路径)
+					Case 保存结果.成功
+						写日志(桌面 & 下载保存成功)
+					Case 保存结果.重复
+						写日志(桌面 & 设置的下载目录中已有此图)
+					Case 保存结果.网站访问失败
+						发通知(桌面网站访问失败通知)
+						写日志(桌面 & 网站访问失败)
+					Case 保存结果.图片下载失败
+						发通知(桌面图片下载失败通知)
+						写日志(桌面 & 图片下载失败)
+					Case 保存结果.不支持该网站
+						发通知(桌面暂不支持该网站通知)
+						写日志(桌面 & 暂不支持该网站)
+					Case 保存结果.没有找到链接
+						发通知(桌面网页解析失败通知)
+						写日志(桌面 & 网页解析失败)
+					Case 保存结果.未定义
+						写日志(桌面 & 无符合要求的原图)
+				End Select
+			Catch ex As IqdbApi.Exceptions.InvalidFileFormatException
+				通知文本 = 桌面 & ex.InnerException.Message
+				自定义通知(通知文本)
+				写日志(通知文本)
+			Catch ex As Net.Http.HttpRequestException
+				通知文本 = 桌面 & ex.InnerException.Message
+				自定义通知(通知文本)
+				写日志(通知文本)
+			End Try
+		End If
+		路径 = 搜索锁屏壁纸(设置项.当前锁屏路径)
+		If String.IsNullOrEmpty(路径) Then
+			发通知(锁屏壁纸搜索失败通知)
+			写日志(锁屏 & 无法定位当前壁纸)
+		Else
+			Try
+				Select Case Await 搜索并筛选原图(路径)
+					Case 保存结果.成功
+						写日志(锁屏 & 下载保存成功)
+					Case 保存结果.重复
+						写日志(锁屏 & 设置的下载目录中已有此图)
+					Case 保存结果.网站访问失败
+						发通知(锁屏网站访问失败通知)
+						写日志(锁屏 & 网站访问失败)
+					Case 保存结果.图片下载失败
+						发通知(锁屏图片下载失败通知)
+						写日志(锁屏 & 图片下载失败)
+					Case 保存结果.不支持该网站
+						发通知(锁屏暂不支持该网站通知)
+						写日志(锁屏 & 暂不支持该网站)
+					Case 保存结果.没有找到链接
+						发通知(锁屏网页解析失败通知)
+						写日志(锁屏 & 网页解析失败)
+					Case 保存结果.未定义
+						写日志(锁屏 & 无符合要求的原图)
+				End Select
+			Catch ex As IqdbApi.Exceptions.InvalidFileFormatException
+				通知文本 = 锁屏 & ex.InnerException.Message
+				自定义通知(通知文本)
+				写日志(通知文本)
+			Catch ex As Net.Http.HttpRequestException
+				通知文本 = 锁屏 & ex.InnerException.Message
+				自定义通知(通知文本)
+				写日志(通知文本)
+			End Try
+		End If
+	End Sub
+End Module
---
+++ 自动搜索下载你的二次元桌面和锁屏原图/图站检索.vb
@@ -0,0 +1,104 @@
+﻿Imports System.IO
+Imports IqdbApi
+Imports IqdbApi.Models
+Imports Aspose
+Enum 保存结果 As Byte
+	未定义
+	成功
+	重复
+	网站访问失败
+	图片下载失败
+	不支持该网站
+	没有找到链接
+End Enum
+
+Friend Module 图站检索
+	Private ReadOnly IQDB客户端 As New IqdbClient '(New Net.Http.HttpClientHandler With {.ServerCertificateCustomValidationCallback = Function() True})
+	Private ReadOnly HTTP客户端 As New Net.Http.HttpClient
+
+	Public Async Function IQDB查询(图片路径 As String) As Task(Of IEnumerable(Of 搜索结果))
+		Using 文件流 As FileStream = File.OpenRead(图片路径)
+			Return From 结果 As Match In (Await IQDB客户端.SearchFile(文件流)).Matches Select New 搜索结果(结果)
+		End Using
+	End Function
+
+	Public Async Function 从网页保存原图(网页URL As String) As Task(Of 保存结果)
+		Dim HTML文档 As Html.HTMLDocument, HTML集合 As Html.Collections.HTMLCollection
+		If 网页URL.Contains("konachan") Then
+			Try
+				HTML文档 = Await Task.Run(Function() New Html.HTMLDocument(网页URL))
+			Catch ex As Exception
+				Return 保存结果.网站访问失败
+			End Try
+			HTML集合 = HTML文档.GetElementsByClassName("original-file-unchanged")
+			If HTML集合.Any Then
+				Dim 文件URL As String = HTML集合(0).GetAttribute("href"), 成功 As Boolean
+				Try
+					成功 = Await 尝试入库(Await HTTP客户端.GetByteArrayAsync(文件URL), If(设置项.扩展名一律设为PNG, Path.GetFileNameWithoutExtension(文件URL) + ".png", Path.GetFileName(文件URL)))
+				Catch ex As Net.Http.HttpRequestException
+					Return 保存结果.图片下载失败
+				End Try
+				Return If(成功, 保存结果.成功, 保存结果.重复)
+			End If
+			HTML集合 = HTML文档.GetElementsByClassName("original-file-changed")
+			If HTML集合.Any Then
+				Dim 文件URL As String = HTML集合(0).GetAttribute("href"), 成功 As Boolean
+				Try
+					成功 = Await 尝试入库(Await HTTP客户端.GetByteArrayAsync(文件URL), If(设置项.扩展名一律设为PNG, Path.GetFileNameWithoutExtension(文件URL) + ".png", Path.GetFileName(文件URL)))
+				Catch ex As Net.Http.HttpRequestException
+					Return 保存结果.图片下载失败
+				End Try
+				Return If(成功, 保存结果.成功, 保存结果.重复)
+			End If
+			Return 保存结果.没有找到链接
+		Else
+			Return 保存结果.不支持该网站
+		End If
+	End Function
+
+	Public Async Function 搜索并筛选原图(路径 As String) As Task(Of 保存结果)
+		Dim 原始结果 As SearchResult
+		If 检查库存(计算哈希(File.ReadAllBytes(路径))) Then
+			Return 保存结果.重复
+		Else
+			Using 文件流 As FileStream = File.OpenRead(路径)
+				原始结果 = Await IQDB客户端.SearchFile(文件流)
+			End Using
+		End If
+		Dim 筛选列表 As IEnumerable(Of (Match, UInteger, UInteger)) = From 结果 As Match In 原始结果.Matches Where 结果.Resolution.Width >= 设置项.宽度不小于 AndAlso 结果.Resolution.Height >= 设置项.高度不小于 AndAlso 结果.Similarity >= 设置项.相似度不小于 AndAlso (结果.MatchType = Enums.MatchType.Best OrElse 结果.MatchType = Enums.MatchType.Additional) Select (结果, Math.Min(结果.Resolution.Height, 结果.Resolution.Width), Math.Max(结果.Resolution.Height, 结果.Resolution.Width))
+		If 筛选列表.Any = False Then
+			Return 保存结果.未定义
+		End If
+		Dim 最大短轴 As UInteger = Aggregate 结果 As (Match, UInteger, UInteger) In 筛选列表 Into Max(结果.Item2)
+		筛选列表 = From 结果 As (Match, UInteger, UInteger) In 筛选列表 Where 结果.Item2 >= 最大短轴
+		If 筛选列表.Any = False Then
+			Return 保存结果.未定义
+		End If
+		Dim 最大长轴 As UInteger = Aggregate 结果 As (Match, UInteger, UInteger) In 筛选列表 Into Max(结果.Item3)
+		Dim Match列表 As IEnumerable(Of Match) = From 结果 As (Match, UInteger, UInteger) In 筛选列表 Where 结果.Item3 >= 最大长轴 Select 结果.Item1
+		Dim 原图尺寸 As Resolution = 原始结果.YourImage.Resolution
+		Dim 结果列表 = If(原图尺寸.Width >= 原图尺寸.Height,
+			From 结果 As Match In Match列表 Where 结果.Resolution.Width >= 结果.Resolution.Height,
+			From 结果 As Match In Match列表 Where 结果.Resolution.Width < 结果.Resolution.Height)
+		If Not 结果列表.Any Then
+			结果列表 = Match列表
+		End If
+		Dim 最终结果 As 保存结果 = 保存结果.未定义, 当前结果 As 保存结果
+		For Each URL As String In From 结果 As Match In 结果列表 Select 搜索结果.URL修饰(结果.Url)
+			当前结果 = Await 从网页保存原图(URL)
+			Select Case 当前结果
+				Case 保存结果.成功
+					最终结果 = 保存结果.成功
+					Exit For
+				Case 保存结果.重复
+					最终结果 = 保存结果.重复
+					Exit For
+				Case Else
+					If 最终结果 = 保存结果.未定义 Then
+						最终结果 = 当前结果
+					End If
+			End Select
+		Next
+		Return 最终结果
+	End Function
+End Module
